47f4543d757ceb9c1a518734018be341cbac4b60
Madiba Hudson-Quansah <mhquansah@gmail.com>
2023-04-11 10:55:11 +0000

    (feat): Made pdf building asynchronous

--- build.sh
+++ build.sh
@@ -7,19 +7,19 @@ cd ./_Raws/Calc || exit 1
 for file in ./*.tex; do
 	echo "$file"
 	filename=$(basename "$file" .tex)
-	latexmk -pdf -synctex=1 -interaction=nonstopmode -outdir="../../_Exported/Calc" "$file" &>"$basedir/latex.log"
+	(
+		latexmk -pdf -synctex=1 -interaction=nonstopmode -outdir="$basedir/_Exported/Calc" "$file" &>"$basedir/latex.log"
+		rm "$basedir/_Exported/Calc/$filename.aux" "$basedir/_Exported/Calc/$filename.fls" "$basedir/_Exported/Calc/$filename.toc" "$basedir/_Exported/Calc/$filename.synctex.gz" "$basedir/_Exported/Calc/$filename.fdb_latexmk" "$basedir/_Exported/Calc/$filename.log" &>/dev/null
+	) &
 done
 cd "$basedir" || exit 1
-
-echo "Cleaning Calc dir"
-rm ./_Exported/Calc/*.aux ./_Exported/Calc/*.fls ./_Exported/Calc/*.toc ./_Exported/Calc/*.synctex.gz ./_Exported/Calc/*.fdb_latexmk ./_Exported/Calc/*.log
 
 # Intro to Computing Info Systems Beyond Notes
 mkdir -p "./_Exported/CompSci/Information Systems For Business and Beyond Notes"
 for file in ./_Raws/CompSci/Information_Systems/*.md; do
 	echo "$file"
 	filename=$(basename "$file" .md)
-	pandoc "$file" -o "./_Exported/CompSci/Information Systems For Business and Beyond Notes/$filename.pdf" --template=./Templates/eisvogel.tex --listings
+	(pandoc "$file" -o "./_Exported/CompSci/Information Systems For Business and Beyond Notes/$filename.pdf" --template=./Templates/eisvogel.tex --listings) &
 done
 
 # Intro to Computing Runestone Notes
@@ -27,7 +27,7 @@ mkdir -p "./_Exported/CompSci/Runestone Notes"
 for file in ./_Raws/CompSci/Runestone/*.md; do
 	echo "$file"
 	filename=$(basename "$file" .md)
-	pandoc "$file" -o "./_Exported/CompSci/Runestone Notes/$filename.pdf" --template=./Templates/eisvogel.tex --listings
+	(pandoc "$file" -o "./_Exported/CompSci/Runestone Notes/$filename.pdf" --template=./Templates/eisvogel.tex --listings) &
 done
 
 # Personal Notes
@@ -36,7 +36,7 @@ mkdir -p "./_Exported/Personal/Intro To Machine Learning"
 for file in ./_Raws/CompSci/Personal/Intro\ To\ Machine\ Learning/*.md; do
 	echo "$file"
 	filename=$(basename "$file" .md)
-	pandoc "$file" -o "./_Exported/Personal/Intro To Machine Learning/$filename.pdf" --template=./Templates/eisvogel.tex --listings
+	(pandoc "$file" -o "./_Exported/Personal/Intro To Machine Learning/$filename.pdf" --template=./Templates/eisvogel.tex --listings) &
 done
 
 # Algorithms
@@ -44,5 +44,5 @@ mkdir -p "./_Exported/Personal/Algorithms"
 for file in ./_Raws/CompSci/Personal/Algorithms/*.md; do
 	echo "$file"
 	filename=$(basename "$file" .md)
-	pandoc "$file" -o "./_Exported/Personal/Algorithms/$filename.pdf" --template=./Templates/eisvogel.tex --listings
+	(pandoc "$file" -o "./_Exported/Personal/Algorithms/$filename.pdf" --template=./Templates/eisvogel.tex --listings) &
 done
